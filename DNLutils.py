b5ff47fc3ca3cff08df7d312ee5a0d1cfd78fdb7
adriansh95 <adrianshestakov@gmail.com>
2023-08-17 22:52:06 -0700

    made a constructor class in DNLutils to share state

--- DNLutils.py
+++ DNLutils.py
@@ -2,12 +2,6 @@ import numpy as np
 from scipy.signal import savgol_filter
 from scipy.stats import chisquare
 
-def buildADCBinsFromDNL(dnl):
-    rightmostEdge = len(dnl+1)
-    bins = np.concatenate(([0], np.cumsum(1+dnl)))
-    bins *= rightmostEdge / bins[-1]
-    return bins
-
 def calculateINL(adcBins):
     idealMids = 0.5 + np.arange(len(adcBins[:-1]))
     adcMids = 0.5 * (adcBins[1:] + adcBins[:-1])
@@ -16,34 +10,140 @@ def calculateINL(adcBins):
 def calculateDNL(adcBins):
     return (adcBins[1:] - adcBins[:-1]) - 1
 
-def buildADCBinsFromDist(measuredDist, expectedDist, minCounts=100, xCutoff=0):
-    useable = expectedDist > minCounts
-    codes = np.arange(len(measuredDist))
-    firstCode = max(codes[useable].min(), xCutoff)
-    lastCode = codes[useable].max()
+def buildADCBinsFromDNL(dnl):
+    rightmostEdge = len(dnl+1)
+    bins = np.concatenate(([0], np.cumsum(1+dnl)))
+    bins *= rightmostEdge / bins[-1]
+    return bins
 
-    if lastCode < firstCode:
-        raise ValueError("firstCode cannot be greater than lastCode. Perhaps xCutoff is too high.")
+def buildADCBinsFromINL(inl):
+    adcRange = 2**18
+    edges = [0]
     
-    preCodes = np.arange(firstCode)
-    workingCodes = np.arange(firstCode, lastCode+1)
-    postCodes = np.arange(lastCode+1, codes.max()+1)
-    
-    if measuredDist[firstCode] > expectedDist[firstCode]:
-        codeWidth = 1 + 2 * ((measuredDist[firstCode] - expectedDist[firstCode])/
-                             (expectedDist[firstCode+1] +
-                              expectedDist[firstCode-1]))
-    else:
-        codeWidth = measuredDist[firstCode]/expectedDist[firstCode]
+    for i, inlVal in enumerate(inl):
+        edges.append(2*(i+1/2-inlVal) - edges[-1])
+        
+    edges = np.array(edges)
+    edges *= (adcRange)/edges[-1]
+    return edges
 
-    edges = np.nan * np.arange(len(workingCodes) + 1)
-    edges[0] = firstCode + 0.5 - codeWidth/2
-    edges[1] = firstCode + 0.5 + codeWidth/2
+class ADCconstructor():
+    def __init__(self, measuredDist, expectedDist, minCounts=100, xCutoff=0):
+        useable = expectedDist > minCounts
+
+        self.measuredDist = measuredDist
+        self.expectedDist = expectedDist
+        self.codes = np.arange(len(measuredDist))
+        self._minCounts = minCounts
+        self._xCutoff = xCutoff
+
+        firstCode = max(self.codes[useable].min(), xCutoff)
+        lastCode = self.codes[useable].max()
+
+        if lastCode < firstCode:
+            raise ValueError("firstCode cannot be greater than lastCode. Perhaps xCutoff is too high.")
+        
+        self.workingCodes = np.arange(firstCode, lastCode+1)
+
+    def buildADCEdges(self, mode="riemann"):
+        measuredDist = self.measuredDist
+        minCounts = self._minCounts
+        xCutoff = self._xCutoff
+        preCodes = np.arange(self.workingCodes[0])
+        postCodes = np.arange(self.workingCodes[-1]+1, self.codes.max()+1)
+       
+        if mode == "riemann":
+            edges  =  self._buildEdgesRiemann()
+        elif mode == "trapazoid":
+            edges  =  self._buildEdgesTrapazoid()
+
+        adcBins = np.concatenate((preCodes, edges, postCodes+1))
+        return adcBins
+
+    def _buildEdgesRiemann(self):
+        codes = self.codes
+        workingCodes = self.workingCodes
+        measuredDist = self.measuredDist
+        expectedDist = self.expectedDist
+
+        firstCode, lastCode = self.workingCodes[0], self.workingCodes[-1]
+
+        if measuredDist[firstCode] > expectedDist[firstCode]:
+            codeWidth = 1 + 2 * ((measuredDist[firstCode] - expectedDist[firstCode])/
+                                 (expectedDist[firstCode+1] +
+                                  expectedDist[firstCode-1]))
+        else:
+            codeWidth = measuredDist[firstCode]/expectedDist[firstCode]
+
+        edges = np.nan * np.arange(len(workingCodes) + 1)
+        edges[0] = firstCode + 0.5 - codeWidth/2
+        edges[1] = firstCode + 0.5 + codeWidth/2
+        
+        for iCode, code in enumerate(workingCodes[1:]):
+            leftEdge = edges[iCode+1]
+            floorLE = int(np.floor(leftEdge))
+            nextInt = floorLE + 1
+
+            forwardArea = self._computeForwardArea(leftEdge)
+           
+            # This is an integer upper bound on where to place the right edge for the code
+            # The right edge will lie between the integers corresponding to 
+            # nextInt+i-2 and nextInt+i-1
+            i = np.argwhere(forwardArea > measuredDist[code]).min()
+            
+            # This is the integrated area under the filtered curve right of the leftEdge
+            # and left of floor(rightEdge). It 0 in the case where floor(rightEdge) < leftEdge
+            c = forwardArea[i-1]
+            
+            # The integrated area under the filtered curve between max(floor(rightEdge), leftEdge)
+            # and rightEdge is: (rightEdge - max(nextInt+i-2, leftEdge)) * 
+            # expectedDist[max(nextInt+i-2, floor(leftEdge))]
+            # Invoking the max function for the case where leftEdge > floor(rightEdge) (c=0 case)\
+            
+            # measuredDist[code] = c + (rightEdge - max(nextInt+i-2, leftEdge) *
+            # expectedDist[max(nextInt+i-2, floor(leftEdge))]) gives
+            
+            rightEdge = ((measuredDist[code] - c)/(expectedDist[max(nextInt+i-2, floorLE)]) +
+                          max(nextInt+i-2, leftEdge))
+                
+            edges[iCode+2] = rightEdge
+
+        return edges
+
+    def _buildEdgesTrapazoid(self):
+        codes = self.codes
+        workingCodes = self.workingCodes
+        measuredDist = self.measuredDist
+        expectedDist = self.expectedDist
+
+        firstCode, lastCode = self.workingCodes[0], self.workingCodes[-1]
+
+        pdf = self.buildPDFfromFilter()
+        slopes = pdf[1:] - pdf[:-1]
+     
+        if measuredDist[firstCode] > expectedDist[firstCode]:
+            a = slope[firstCode+1] - slope[firstCode-1]
+            b = 2*(pdf[firstCode+1] + pdf[firstCode])
+            c = pdf[firstCode+1] + pdf[firstCode] - 2*measuredDist[firstCode]
+
+            dx = (-b + np.sqrt(b**2 - 4*a*c)) / (2*a)
+
+            codeWidth = 1 + 2*dx
+        else:
+            codeWidth = measuredDist[firstCode]/expectedDist[firstCode]
     
-    for iCode, code in enumerate(workingCodes[1:]):
-        leftEdge = edges[iCode+1]
-        floorLE = int(np.floor(leftEdge))
+        edges = np.nan * np.arange(len(workingCodes) + 1)
+        edges[0] = firstCode + 0.5 - codeWidth/2
+        edges[1] = firstCode + 0.5 + codeWidth/2
+
+        for iCode, code in enumerate(workingCodes[1:]):
+            leftEdge = edges[iCode+1]
+            floorLE = int(np.floor(leftEdge))
+            nextInt = int(np.ceil(leftEdge))
+
+    def _computeForwardArea(self, leftEdge):
         nextInt = int(np.ceil(leftEdge))
+        expectedDist = self.expectedDist
         
         # The cumsum of this is the area under the filtered curve to the right
         # of the left edge for this code
@@ -52,32 +152,17 @@ def buildADCBinsFromDist(measuredDist, expectedDist, minCounts=100, xCutoff=0):
         dForwardArea = np.concatenate(([0], [(nextInt-leftEdge)*expectedDist[nextInt-1]],
                                       expectedDist[nextInt:]))
         forwardArea = np.cumsum(dForwardArea)
+        return forwardArea
+
+    def buildPDFfromFilter(self):
+        filteredDist = self.expectedDist
+        pdf = np.zeros(len(filteredDist)+1)
+
+        for i, f in enumerate(filteredDist):
+            pdf[i+1] = 2*f - pdf[i]
         
-        # This is an integer upper bound on where to place the right edge for the code
-        # The right edge will lie between the integers corresponding to 
-        # nextInt+i-2 and nextInt+i-1
-        i = np.argwhere(forwardArea > measuredDist[code]).min()
-        
-        # This is the integrated area under the filtered curve right of the leftEdge
-        # and left of floor(rightEdge). It 0 in the case where floor(rightEdge) < leftEdge
-        c = forwardArea[i-1]
-        
-        # The integrated area under the filtered curve between max(floor(rightEdge), leftEdge)
-        # and rightEdge is: (rightEdge - max(nextInt+i-2, leftEdge)) * 
-        # expectedDist[max(nextInt+i-2, floor(leftEdge))]
-        # Invoking the max function for the case where leftEdge > floor(rightEdge) (c=0 case)\
-        
-        # measuredDist[code] = c + (rightEdge - max(nextInt+i-2, leftEdge) *
-        # expectedDist[max(nextInt+i-2, floor(leftEdge))]) gives
-        
-        rightEdge = ((measuredDist[code] - c)/(expectedDist[max(nextInt+i-2, floorLE)]) +
-                      max(nextInt+i-2, leftEdge))
-            
-        edges[iCode+2] = rightEdge
-    
-    adcBins = np.concatenate((preCodes, edges, postCodes+1))
-    return adcBins
-    
+        return pdf
+
 def periodicDNL(bins, amp=0.1):
     n = 18
     bits = np.arange(n)
@@ -92,17 +177,6 @@ def sinusoid(amp=1, freq=1):
     x = np.arange(adcRange)
     y = amp * np.sin(freq * x)
     return y
-
-def buildADCBinsFromINL(inl):
-    adcRange = 2**18
-    edges = [0]
-    
-    for i, inlVal in enumerate(inl):
-        edges.append(2*(i+1/2-inlVal) - edges[-1])
-        
-    edges = np.array(edges)
-    edges *= (adcRange)/edges[-1]
-    return edges
 
 def applyADCtoDistributionBasic(adcBins, distribution):
     dnl = calculateDNL(adcBins)
